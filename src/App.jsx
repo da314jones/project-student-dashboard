dc35450d322c74beb577c25f2ccc1f4f136e1da6
da314jones <dwaynejones@pursuit.org>
2023-08-07 19:24:16 -0400

    Rendered student  data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import data from "./data/data.json"
+
 
 function App() {
+  console.log(data)
   return (
     <div>
       <h1>Student Dashboard</h1>
+      <ul>
+      {data.map((student) => (
+          <li key={student.id}>{student.names.preferredName}</li>
+        ))}
+      </ul>
     </div>
   );
 }
