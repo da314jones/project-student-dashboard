9aee514b4562b9e866e6e496fc05f7ccfbc1324e
da314jones <dwaynejones@pursuit.org>
2023-08-08 00:59:24 -0400

    Rendered cohorts list sort by cohort

--- src/Components/CohortList.jsx
+++ src/Components/CohortList.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default function CohortList() {
+export default function CohortList({cohorts, onCohortClick}) {
+  
+  const cohortGroups = cohorts.reduce((result, cohortCode) => {
+    const [season, year] = cohortCode.split(/(\d+)/).filter(Boolean);
+    const readableCohort = `${season} ${year}`;
+
+    if ( !result[readableCohort]) {
+      result[readableCohort] =[];
+    }
+    result[readableCohort].push(cohortCode);
+    return result;
+  }, {});
+
+
   return (
     <div>
-      
+      <button onClick={() => onCohortClick("all")}>All Student</button>
+      {Object.keys(cohortGroups).map((cohort, index) => (
+        <button key={index} onClick={() => onCohortClick(cohort)}>{cohort}</button>
+      ))}
     </div>
   )
 }
