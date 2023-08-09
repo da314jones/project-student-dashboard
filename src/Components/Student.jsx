d8bfbc49d5185d0b629dda739389b81ca9cc0e48
da314jones <dwaynejones@pursuit.org>
2023-08-08 22:47:24 -0400

    Functionality for the details & notes show buttons

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -3,16 +3,19 @@ import StudentDetails from './StudentDetails'
 import "./Student.css"
 
 export default function Student({student, onAddNote, notes}) {
-  const [showMore, setShowMore] = useState(false)
-    
+  const [showMore, setShowMore] = useState(false);
+  const toggleDetails = () => {
+    setShowMore(prevShowMore => !prevShowMore);
+  }
+      
   return (
     <div className='student' >
       <img src={student.profilePhoto} alt={`${student.names.preferredName}'s profile`} />
       <p>{student.names.preferredName}</p>
       <p>{student.username}</p>
       <p>{student.dob}</p>
-      <button id="show-more">Show More...</button>
-      <StudentDetails details={student} onAddNote={onAddNote} notes={notes} />
+      <button onClick={toggleDetails} id="show-more">Show More...</button>
+      {showMore && <StudentDetails  showMore={showMore} details={student} onAddNote={onAddNote} notes={notes} />}
     </div>
   )
 }
--- src/Components/StudentDetails.jsx
+++ src/Components/StudentDetails.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { useState } from "react";
 import Note from "./Note";
 import "./StudentDetails.css";
 
-export default function StudentDetails({ details, studentId, onAddNote, notes }) {
-
+export default function StudentDetails({ details, studentId, onAddNote, notes, showMore }) {
+const [showNotes, setShowNotes] = useState(false);
+const toggleNotes = () => {
+  setShowNotes(prevShowNotes => !prevShowNotes)
+}
   const total = details.codewars.current.total;
   const goal = details.codewars.goal.total;
   const percentage = ((total / goal) * 100).toFixed(0);
@@ -27,8 +30,8 @@ const certifications = (details.cohort.scores.assessments * 100)
       <p>LinkedIn: {details.certifications.linkedin}</p>
       <p>Mock Interview: {details.certifications.github}</p>
       <p>Git Hub: {details.certifications.mockInterview}</p>
-      <button id="note-button">Notes</button>
-      <Note studentId={studentId} onAddNote={onAddNote} notes={notes} />
+      <button onClick={toggleNotes} id="note-button">Notes</button>
+      {showNotes && <Note showNotes={showNotes} studentId={studentId} onAddNote={onAddNote} notes={notes} />}
     </div>
   );
 }
