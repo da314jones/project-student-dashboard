20f9dba0ae4f793a6f1cdbc5cb624f3804d7c2df
da314jones <dwaynejones@pursuit.org>
2023-08-15 00:47:32 -0400

    note clean up

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as mdb from 'mdb-ui-kit'; // lib
-import { Input } from 'mdb-ui-kit'; // module
+import * as mdb from 'mdb-ui-kit'; 
+import { Input } from 'mdb-ui-kit';
 
 import App from "./App";
 
